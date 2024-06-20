8dadaf516b8f257490c0781d2319a739d22a1eeb
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-20 12:22:19 -0300

    ProductsController e implemente os métodos para obter todos os produtos e obter um produto por ID.

---
+++ StockApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await _productService.GetProducts();
+            if (products == null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound("Product not Found");
+            }
+            return Ok(product);
+        }
+    }
+}
