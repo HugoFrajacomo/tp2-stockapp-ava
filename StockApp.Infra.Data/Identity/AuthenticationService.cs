cb12356fc1d6e604105338e889b9bddd749085c4
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-06-22 18:55:31 -0300

    Sistema de autenticação

--- StockApp.Infra.Data/Identity/AuthenticationService.cs
+++ StockApp.Infra.Data/Identity/AuthenticationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using StockApp.Domain.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,44 @@ using System.Threading.Tasks;
 
 namespace StockApp.Infra.Data.Identity
 {
-    public class AuthenticationService
+    public class AuthenticateService : IAuthenticate
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthenticateService(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = email,
+                Email = email
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(applicationUser, isPersistent: false);
+            }
+            return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
     }
 }
+
