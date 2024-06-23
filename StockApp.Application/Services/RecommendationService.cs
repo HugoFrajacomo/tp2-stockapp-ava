801a73172f63cae1a07eb2e614511f92d05f7280
carloosramos <carlos.ramos6@fatec.sp.gov.br>
2024-06-23 00:23:34 -0300

    Implementar Mecanismo de Cache Distribuído com Redis

--- StockApp.Application/Services/RecommendationService.cs
+++ StockApp.Application/Services/RecommendationService.cs
@@ -1,30 +1,44 @@
-﻿using StockApp.Domain.Entities;
-using StockApp.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using StockApp.Domain.Interfaces;
+using StockApp.Domain.Entities;
 
 namespace StockApp.Application.Services
 {
     public class RecommendationService : IRecommendationService
     {
+        private readonly IDistributedCache _cache;
         private readonly IOrderRepository _orderRepository;
 
-        public RecommendationService(IOrderRepository orderRepository)
+        public RecommendationService(IDistributedCache cache, IOrderRepository orderRepository)
         {
+            _cache = cache;
             _orderRepository = orderRepository;
         }
 
         public async Task<IEnumerable<Product>> GetRecommendationsAsync(string userId)
         {
+            string cacheKey = $"recommendations_{userId}";
+            string cachedRecommendations = await _cache.GetStringAsync(cacheKey);
+
+            if (!string.IsNullOrEmpty(cachedRecommendations))
+            {
+                return JsonSerializer.Deserialize<IEnumerable<Product>>(cachedRecommendations);
+            }
+
             var userOrders = await _orderRepository.GetByUserIdAsync(userId);
             var recommendedProducts = userOrders.SelectMany(order => order.Products)
                                                 .GroupBy(product => product.Id)
                                                 .OrderByDescending(group => group.Count())
                                                 .Select(group => group.First())
                                                 .Take(5);
+
+            var cacheOptions = new DistributedCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+
+            await _cache.SetStringAsync(cacheKey, JsonSerializer.Serialize(recommendedProducts), cacheOptions);
 
             return recommendedProducts;
         }
