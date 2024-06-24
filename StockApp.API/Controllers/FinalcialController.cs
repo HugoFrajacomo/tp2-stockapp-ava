30b48d5b0143882f271d60c7a6873f3327490162
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-24 10:57:15 -0300

    Implementar Sistema de Gestão de Recursos Financeiros

---
+++ StockApp.API/Controllers/FinalcialController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    public class FinalcialController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class FinancialController : ControllerBase
+        {
+            private readonly IFinancialManagementService _financialService;
+
+            public FinancialController(IFinancialManagementService financialService)
+            {
+                _financialService = financialService;
+            }
+
+            [HttpGet("report")]
+            public async Task<IActionResult> GenerateReport()
+            {
+                var result = await _financialService.GenerateReportAsync();
+                return Ok(result);
+            }
+        }
+    }
+}
